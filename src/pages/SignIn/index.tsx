f111b0e37b7ebed42db10483cc92109e3e341697
Adeonir Kohl <adeonir@gmail.com>
2021-01-15 00:17:19 -0300

    Update signin page adding input and button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Image } from 'react-native'
 
+import Button from 'ui/Button'
+import Input from 'ui/Input'
+
 import logoImg from 'assets/logo.png'
 
 import * as S from './styles'
@@ -11,6 +14,11 @@ const SignIn = () => {
       <Image source={logoImg} />
 
       <S.Title>Faça seu login</S.Title>
+
+      <Input name='email' icon='mail' placeholder='E-mail' />
+      <Input name='password' icon='lock' placeholder='Senha' />
+
+      <Button onPress={() => console.log('Entrar')}>Entrar</Button>
     </S.Container>
   )
 }
