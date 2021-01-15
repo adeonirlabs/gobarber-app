602686c3e5f307627b0d0a2bd0f3ab850185e880
Adeonir Kohl <adeonir@gmail.com>
2021-01-14 23:03:37 -0300

    Add text and style to signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Image } from 'react-native'
+import logoImg from '../../assets/logo.png'
 
 import * as S from './styles'
 
 const SignIn = () => {
-  return <S.Container />
+  return (
+    <S.Container>
+      <Image source={logoImg} />
+
+      <S.Title>Faça seu login</S.Title>
+    </S.Container>
+  )
 }
 
 export default SignIn
