29b150861f753d1146f05f57f7ca704fc66a0452
Adeonir Kohl <adeonir@gmail.com>
2021-01-16 12:57:57 -0300

    Update signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Image } from 'react-native'
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
 import Button from 'ui/Button'
 import Input from 'ui/Input'
@@ -10,16 +17,42 @@ import * as S from './styles'
 
 const SignIn = () => {
   return (
-    <S.Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={{ flex: 1 }}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps='handled'
+        >
+          <S.Container>
+            <Image source={logoImg} />
 
-      <S.Title>Faça seu login</S.Title>
+            <View>
+              <S.Title>Faça seu login</S.Title>
+            </View>
 
-      <Input name='email' icon='mail' placeholder='E-mail' />
-      <Input name='password' icon='lock' placeholder='Senha' />
+            <Input name='email' icon='mail' placeholder='E-mail' />
+            <Input name='password' icon='lock' placeholder='Senha' />
 
-      <Button onPress={() => console.log('Entrar')}>Entrar</Button>
-    </S.Container>
+            <Button onPress={() => console.log('Entrar')}>Entrar</Button>
+
+            <S.ForgotPassword
+              onPress={() => console.log('esqueci minha senha')}
+            >
+              <S.ForgotPasswordText>Esqueci minha senha</S.ForgotPasswordText>
+            </S.ForgotPassword>
+          </S.Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <S.CreateAccountButton onPress={() => console.log('criar uma conta')}>
+        <Icon name='log-in' size={20} color='#ff9000' />
+        <S.CreateAccountButtonText>Criar uma conta</S.CreateAccountButtonText>
+      </S.CreateAccountButton>
+    </>
   )
 }
 
