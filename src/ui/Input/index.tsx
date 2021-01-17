3396f8f9f40da19b3b4d283e225b5766a3393a08
Adeonir Kohl <adeonir@gmail.com>
2021-01-17 08:50:40 -0300

    Add focus and blur states to input

--- src/ui/Input/index.tsx
+++ src/ui/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useCallback, useState } from 'react'
 import { TextInput, TextInputProps } from 'react-native'
 
 import * as S from './styles'
@@ -9,12 +9,30 @@ type InputProps = {
 
 const Input = forwardRef<TextInput, InputProps>(
   ({ icon, ...props }, forwardedRef) => {
+    const [isFocused, setIsFocused] = useState(false)
+
+    const handleInputFocus = useCallback(() => {
+      setIsFocused(true)
+    }, [])
+
+    const handleInputBlur = useCallback(() => {
+      setIsFocused(false)
+    }, [])
+
     return (
-      <S.Container>
-        {icon && <S.Icon name={icon} size={20} color='#666360' />}
+      <S.Container isFocused={isFocused}>
+        {icon && (
+          <S.Icon
+            name={icon}
+            size={20}
+            color={isFocused ? '#ff9000' : '#666360'}
+          />
+        )}
 
         <S.TextInput
           ref={forwardedRef}
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
           keyboardAppearance='dark'
           placeholderTextColor='#666360'
           {...props}
--- src/ui/Input/styles.ts
+++ src/ui/Input/styles.ts
@@ -1,15 +1,31 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 import FeatherIcon from 'react-native-vector-icons/Feather'
 
-export const Container = styled.View`
-  width: 100%;
-  height: 60px;
-  padding: 0 16px;
-  background: #232129;
-  border-radius: 10px;
-  margin-bottom: 8px;
-  flex-direction: row;
-  align-items: center;
+type ContainerProps = {
+  isFocused: boolean
+}
+
+const modifier = {
+  isFocused: () => css`
+    border-color: #ff9000;
+  `,
+}
+
+export const Container = styled.View<ContainerProps>`
+  ${({ isFocused }) => css`
+    width: 100%;
+    height: 60px;
+    padding: 0 16px;
+    background: #232129;
+    border-width: 1px;
+    border-color: #232129;
+    border-radius: 10px;
+    margin-bottom: 8px;
+    flex-direction: row;
+    align-items: center;
+
+    ${isFocused && modifier.isFocused()}
+  `}
 `
 
 export const TextInput = styled.TextInput`
