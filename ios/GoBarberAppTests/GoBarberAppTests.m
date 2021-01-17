1fe46033d429c596a2de8d9e7c4d44799307ee1a
Adeonir Kohl <adeonir@gmail.com>
2021-01-16 23:23:13 -0300

    Rename app

--- ios/GoBarberAppTests/GoBarberAppTests.m
+++ ios/GoBarberAppTests/GoBarberAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface gobargerappTests : XCTestCase
+@interface GoBarberAppTests : XCTestCase
 
 @end
 
-@implementation gobargerappTests
+@implementation GoBarberAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
